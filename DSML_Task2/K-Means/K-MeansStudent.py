8a3f06b8b839e9a986c821df90f0d98251c10a77
clara <clara.monaco.95@gmail.com>
2019-08-21 19:02:47 +0200

    modifiche Kmeans

--- DSML_Task2/K-Means/K-MeansStudent.py
+++ DSML_Task2/K-Means/K-MeansStudent.py
@@ -2,62 +2,12 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import os
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+
 import pandas as pd
 import csv
 import json
 from sklearn.preprocessing import LabelEncoder
-
-
-
-""" -------------------------genera file studenti----------------------------"""
-
-DEGREE_PATH= "C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv"
-
-def load_degree_data(degree_path=DEGREE_PATH):
-    csv_path = os.path.join(degree_path, "C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv")
-    return pd.read_csv(csv_path)
-
-degree_db = load_degree_data()
-laureati = [tuple(row) for row in csv.reader(open('C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv', 'r'))]
-tip_mat= json.load(open('C:/Users/clara/PycharmProjects/prog2/DSML/FileGenerated/Tipo_mat.txt'))
-print(tip_mat)
-Maturità = []
-Maturitàtemp = []
-MaturitàDict= {}
-SchoolDict = {}
-nome_Maturità = " "
-
-Total_Write = []
-Total_Temp = []
-classe_maturita = 0
-matricola =[]
-
-for i in range(1, len(laureati)):
-    matricola_studente=int(laureati[i][0])
-   # classe_maturita = int(laureati[i][21])
-    num=len(laureati[i][13])
-    stringa=(laureati[i][13][1:num-1])
-   # stringa="\""+stringa+"\""
-
-   # print(tip_mat.get(stringa))
-    tipo_maturità=tip_mat.get(stringa)
-    voto_diploma = int(laureati[i][11])
-    fuori_corso = int (laureati[i][20])
-    if fuori_corso!=0:
-      fuori_corso=1
-
-    CFU_primo = int(laureati[i][2])
-    if CFU_primo != -1:
-       if CFU_primo <= 60:
-        Maturitàtemp = [[matricola_studente,tipo_maturità, voto_diploma, CFU_primo, fuori_corso]]# da classe a tipo
-        Maturità.append(Maturitàtemp)
-
-
-
-
-
-with open('C:/Users/clara/PycharmProjects/prog2/DSML_Task2/K-Means/ListStudent2.txt', 'w') as file:
-    file.write(json.dumps(Maturità))
 
 
 
@@ -88,7 +38,27 @@ le=LabelEncoder()
 df = DataFrame(Data, columns=[ 'TipoMaturità', 'VotoDiploma','CFU1', 'AnniFuoriCorso'])
 
 print(df)
-kmeans = KMeans(n_clusters=6).fit(df)
+kmeans = KMeans(n_clusters=4).fit(df)
 centroids = kmeans.cluster_centers_
+#dt={'TipoMaturità': 5,'VotoDiploma': 80,'CFU1':40, 'AnniFuoriCorso':0}
+#kmeans.predict(dt)
+print(kmeans.labels_)
 print(centroids)
+#print(df['TipoMaturità'])
+
+#plt.scatter(df['CFU1'],df['AnniFuoriCorso'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+#plt.scatter(centroids[:,0],centroids[:,1],c='red', s=50)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(df['VotoDiploma'],df['CFU1'],  df['AnniFuoriCorso'],c= kmeans.labels_.astype(float), s=50, alpha=0.5 )
+ax.scatter(centroids[:,1],centroids[:,2],centroids[:,3],c='red', s=50)
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
+plt.show()
 
---
+++ DSML_Task2/K-Means/WriteListStudent2.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+import csv
+import json
+from sklearn.preprocessing import LabelEncoder
+
+
+
+""" -------------------------genera file studenti----------------------------"""
+
+DEGREE_PATH= "C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv"
+
+def load_degree_data(degree_path=DEGREE_PATH):
+    csv_path = os.path.join(degree_path, "C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv")
+    return pd.read_csv(csv_path)
+
+degree_db = load_degree_data()
+laureati = [tuple(row) for row in csv.reader(open('C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv', 'r'))]
+tip_mat= json.load(open('C:/Users/clara/PycharmProjects/prog2/DSML/FileGenerated/Tipo_mat.txt'))
+cod_school=json.load(open('C:/Users/clara/PycharmProjects/prog2/DSML/DecisionTree/Cod_school.txt'))
+print(tip_mat)
+Maturità = []
+Maturitàtemp = []
+MaturitàDict= {}
+SchoolDict = {}
+nome_Maturità = " "
+
+Total_Write = []
+Total_Temp = []
+classe_maturita = 0
+matricola =[]
+
+for i in range(1, len(laureati)):
+    matricola_studente=int(laureati[i][0])
+   # classe_maturita = int(laureati[i][21])
+    num=len(laureati[i][13])
+    stringa=(laureati[i][13][1:num-1])
+   # stringa="\""+stringa+"\""
+
+   # print(tip_mat.get(stringa))
+    tipo_maturità=tip_mat.get(stringa)
+    voto_diploma = int(laureati[i][11])
+    fuori_corso = int (laureati[i][20])
+    """num = len(laureati[i][12])
+       stringa = (laureati[i][12][1:num - 1])
+       codice_mec=cod_school.get(stringa)
+       print(codice_mec)
+    if fuori_corso!=0:
+      fuori_corso=1"""
+
+    CFU_primo = int(laureati[i][2])
+    if CFU_primo != -1:
+       if CFU_primo <= 60:
+        Maturitàtemp = [[matricola_studente,tipo_maturità, voto_diploma, CFU_primo, fuori_corso]]# da classe a tipo
+        Maturità.append(Maturitàtemp)
+
+
+
+
+
+with open('C:/Users/clara/PycharmProjects/prog2/DSML_Task2/K-Means/ListStudent2.txt', 'w') as file:
+    file.write(json.dumps(Maturità))
