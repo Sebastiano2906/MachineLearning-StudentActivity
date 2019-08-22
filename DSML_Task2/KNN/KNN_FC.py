927100444b5a300ef54f836be19539cfe2c09023
clara <clara.monaco.95@gmail.com>
2019-08-22 20:21:55 +0200

    Realizzazione del KNN prima con liststudent3 che ha fuori corso tra 0 e 3, poi ho usato liststudent4 con fuori corso 0 o 1. ne segue che poichè esistono troppi 0 l'algoritmo non riesce a fare una buona previsione. ATTENZIONE il file wriete scrive le liste ma devi pulirle a mano in formato csv

---
+++ DSML_Task2/KNN/KNN_FC.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import os
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import csv
+import json
+
+from sklearn.preprocessing import LabelEncoder
+triennali="C:/Users/clara/PycharmProjects/prog2/DSML_Task2/KNN/ListaStudenti4.csv" #ATTENZIONE AL PATH
+# Assign colum names to the dataset
+names = [ 'TipoMaturità', 'VotoDiploma','CFU1', 'AnniFuoriCorso']
+
+# Read dataset to pandas dataframe
+dataset = pd.read_csv(triennali)
+print(dataset)
+
+#Il prossimo passo è dividere il nostro set di dati nei suoi attributi ed etichette.
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+print("vediamo",X)
+print("la tua y",y)
+
+#Per creare training e divisioni di test, eseguire il seguente script:
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+#Non esiste un valore ideale per K ed è selezionato dopo il test e la valutazione, tuttavia per iniziare, 5 sembra essere il valore più comunemente usato per l'algoritmo KNN.
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=8)
+arra=np.array([3,72,18])
+arra = arra.reshape(1,-1)
+classifier.fit(X_train, y_train)
+print("predict",classifier.predict(arra))
+
+#Il passo finale è fare previsioni sui nostri dati di test. Per fare ciò, eseguire il seguente script:
+y_pred = classifier.predict(X_test)
+
+
+#Per valutare un algoritmo, matrice di confusione, precisione, richiamo e punteggio f1 sono le metriche più comunemente utilizzate.
+# I metodi confusion_matrixe classification_reportdel sklearn.metricspossono essere usati per calcolare queste metriche.
+# Dai un'occhiata al seguente script:
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+"""Nella sezione di addestramento e previsione abbiamo detto che non c'è modo di sapere in anticipo quale valore di K produce i migliori risultati al primo tentativo. Abbiamo scelto casualmente 5 come valore K e ci risulta solo una precisione del 100%.
+
+Un modo per aiutarti a trovare il valore migliore di K è quello di tracciare il grafico del valore K e il tasso di errore corrispondente per il set di dati.
+
+In questa sezione, tracciamo l'errore medio per i valori previsti del set di test per tutti i valori K compresi tra 1 e 40.
+
+Per fare ciò, calcoliamo prima la media dell'errore per tutti i valori previsti in cui K è compreso tra 1 e 40. Esegui il seguente script:"""
+
+
+error = []
+
+# Calculating error for K values between 1 and 40
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error.append(np.mean(pred_i != y_test))
+"""Lo script sopra esegue un ciclo da 1 a 40. In ogni iterazione viene calcolato l'errore medio per i valori previsti del set di test e il risultato viene aggiunto errorall'elenco.
+
+Il prossimo passo è tracciare i errorvalori rispetto ai valori K. Eseguire il seguente script per creare la trama:"""
+
+plt.figure(figsize=(12, 6))
+plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',
+         markerfacecolor='blue', markersize=10)
+plt.title('Error Rate K Value')
+plt.xlabel('K Value')
+plt.ylabel('Mean Error')
+
+plt.show()
---
+++ DSML_Task2/KNN/WriteListStudent3.py
@@ -0,0 +1,49 @@
+"""L'algoritmo K-neighbors neighbors (KNN) è un tipo di algoritmi di apprendimento automatico supervisionato.
+svolge compiti di classificazione piuttosto complessi.
+È un algoritmo di apprendimento pigro poiché non ha una fase di allenamento specializzata. Piuttosto,
+utilizza tutti i dati per l'addestramento mentre classifica un nuovo punto dati o istanza.
+KNN è un algoritmo di apprendimento non parametrico, il che significa che non assume nulla sui dati sottostanti."""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import os
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import csv
+import json
+
+"""-----------------------ATTENZIONE NON RILANCIARE MI è SERVITO PER IL CSV----------------------"""
+
+from sklearn.preprocessing import LabelEncoder
+laureati = [tuple(row) for row in csv.reader(open('C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv', 'r'))]
+Maturità = []
+tip_mat= json.load(open('C:/Users/clara/PycharmProjects/prog2/DSML/FileGenerated/Tipo_mat.txt'))
+cod_school=json.load(open('C:/Users/clara/PycharmProjects/prog2/DSML/DecisionTree/Cod_school.txt'))
+
+
+for i in range(1, len(laureati)):
+    num=len(laureati[i][13])
+    stringa=(laureati[i][13][1:num-1])
+    tipo_maturità=tip_mat.get(stringa)
+    voto_diploma = int(laureati[i][11])
+    fuori_corso = int (laureati[i][20])
+    """num = len(laureati[i][12])
+       stringa = (laureati[i][12][1:num - 1])
+       codice_mec=cod_school.get(stringa)
+       print(codice_mec)"""
+    if fuori_corso!=0:
+      fuori_corso=1
+
+    CFU_primo = int(laureati[i][2])
+    if CFU_primo != -1:
+       if CFU_primo <= 60:
+        Maturitàtemp = [tipo_maturità, voto_diploma, CFU_primo, fuori_corso]# da classe a tipo
+        Maturità.append(Maturitàtemp)
+
+
+df = pd.DataFrame(data={"TipoMaturità, VotoDiploma,CFU1, AnniFuoriCorso": Maturità})
+df.to_csv("./ListaStudenti4.csv", sep=',', index=False,)
+
