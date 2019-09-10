43a4ceff61aed1ba62feed58d4172ccf43fcaee7
clara <clara.monaco.95@gmail.com>
2019-09-10 13:01:17 +0200

    Aggiunta di alcune modifiche

---
+++ DSML_Task2/Clara/Analisys.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import os
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import csv
+import json
+STUDENT_PATH="C:/Users/clara/PycharmProjects/prog2/DSML_Task2/Dataset/DatasetTriennali.csv"
+#STUDENT_PATH="C:/Users/clara/PycharmProjects/prog2/DSML_Task2/KNN/ListaStudenti4.csv"
+#trasforma csv in DataFrame
+student =pd.read_csv(STUDENT_PATH)
+
+
+#tolgo ciò che non mi interessa e trasformo le stringhe in valori numerici
+student=student.drop( ['Matr','CF','2','3','tot','CDS','Tipo_CDS','Coorte','Anni_Carriera','ANNO_DIPLOMA','CODICE_MECCANOGRAFICO','ANNO_ACCADEMICO_LAUREA','VOTO_LAUREA','Erasmus','TESI_ESTERO','STATO_STUDENTE','MOTIVO_STATO_STUDENTE','CLASSE'],axis=1)
+le=LabelEncoder()
+student['TIPO_MATURITA']=le.fit(student['TIPO_MATURITA']).transform(student['TIPO_MATURITA'])
+#print("mostro il mio dataframe\n",student)
+
+
+
+
+
+#mostra info del dataframe
+print("mostro info dettagli struttura\n",student.info())
+#mostra descrizione statistiche
+print("mostro prima dati statistici\n",student.describe())
+#conta per tipomaturità
+print("contiamo per tipo di maturità\n",student["TIPO_MATURITA"].value_counts())
+#matplotlib inline # only in a Jupyter notebook
+"""
+student.hist(bins=50, figsize=(20,15))
+plt.show()
+
+
+#dividi test e trein
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+
+train_set, test_set = split_train_test(student, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
+
+"""
+
+#divido per id
+import hashlib
+def test_set_check(identifier, test_ratio, hash):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column, hash=hashlib.md5):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio, hash))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+student_with_id = student.reset_index() # adds an `index` column
+train_set, test_set = split_train_test_by_id(student_with_id, 0.2, "index")
+
+print(len(train_set), "train +", len(test_set), "test")
+
+"""from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(student, test_size=0.2, random_state=42)"""
+
+student2= student
+#questa è la distribuzione
+student.plot(kind="scatter", x="TIPO_MATURITA", y="VOTO_DIPLOMA")
+#così vedi dove è più denso
+student.plot(kind="scatter", x="TIPO_MATURITA", y="VOTO_DIPLOMA", alpha=0.1)
+#plt.show()
+
+
+corr_matrix = student2.corr()
+print(corr_matrix['TIPO_MATURITA'].sort_values(ascending=False))
+
--- DSML_Task2/K-Means/K-MeansStudent.py
+++ DSML_Task2/K-Means/K-MeansStudent.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import os
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
+import numpy as np
 import pandas as pd
 import csv
 import json
@@ -15,8 +15,10 @@ Maturità = json.load(open("C:/Users/clara/PycharmProjects/prog2/DSML_Task2/K-Me
 tipo_maturità=[]
 voto_diploma =[]
 cfu_primo =[]
+mat_student=[]
 fc=[]
 for i in range(0, len(Maturità)):
+   #mat_student.append(int(Maturità[i][0][0]))
    tipo_maturità.append(int(Maturità[i][0][1]))  # primo volore predittivo
    voto_diploma.append(int(Maturità[i][0][2]))  # secondo valore predittivo
    cfu_primo.append( int(Maturità[i][0][3]))
@@ -26,6 +28,7 @@ for i in range(0, len(Maturità)):
 #   print(tipo_maturità[i])
 
 Data = {
+   # 'Matricola':mat_student,
     'TipoMaturità': tipo_maturità,
     'VotoDiploma': voto_diploma,
     'CFU1':cfu_primo,
@@ -35,30 +38,112 @@ le=LabelEncoder()
 
 
 
-df = DataFrame(Data, columns=[ 'TipoMaturità', 'VotoDiploma','CFU1', 'AnniFuoriCorso'])
+df = DataFrame(Data, columns=['TipoMaturità', 'VotoDiploma','CFU1', 'AnniFuoriCorso'])
+df2=df
+df2=df2.drop(columns="AnniFuoriCorso")
+#print(df2)
 
-print(df)
-kmeans = KMeans(n_clusters=4).fit(df)
+kmeans = KMeans(n_clusters=4).fit(df2)
 centroids = kmeans.cluster_centers_
-#dt={'TipoMaturità': 5,'VotoDiploma': 80,'CFU1':40, 'AnniFuoriCorso':0}
-#kmeans.predict(dt)
+
+
+
+
 print(kmeans.labels_)
 print(centroids)
 #print(df['TipoMaturità'])
+label=np.array(kmeans.labels_)
 
-#plt.scatter(df['CFU1'],df['AnniFuoriCorso'], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
-#plt.scatter(centroids[:,0],centroids[:,1],c='red', s=50)
 
+
+DataFrameFinale= {
+   # 'Matricola':mat_student,
+    'TipoMaturità': tipo_maturità,
+    'VotoDiploma': voto_diploma,
+    'CFU1':cfu_primo,
+    'AnniFuoriCorso':fc,
+    'Cluster':kmeans.labels_
+    }
+le=LabelEncoder()
+
+
+
+df_final = DataFrame(DataFrameFinale, columns=['TipoMaturità', 'VotoDiploma','CFU1', 'AnniFuoriCorso','Cluster'])
+
+#print("-------------------------------------PROVA---------------------------------------------")
+#print(df_final)
+
+dt=[[11,80,30]]
+
+print(kmeans.predict(dt))
+count0=0
+countFc0 =0
+count1 =0
+countFc1 =0
+count2 =0
+countFc2 =0
+count3 = 0
+countFc3 =0
+
+media0=0
+media1=0
+media2=0
+media3=0
+
+
+
+
+
+
+
+"""
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+x = np.array(df2['TipoMaturità'])
+y = np.array(df2['VotoDiploma'])
+z = np.array(df2['CFU1'])
+ax.set_xlabel('TipoMaturità')
+ax.set_ylabel('VotoDiploma')
+ax.set_zlabel('CFU1')
 
-ax.scatter(df['VotoDiploma'],df['CFU1'],  df['AnniFuoriCorso'],c= kmeans.labels_.astype(float), s=50, alpha=0.5 )
-ax.scatter(centroids[:,1],centroids[:,2],centroids[:,3],c='red', s=50)
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+ax.scatter(x,y,z, marker="s", c=kmeans.labels_.astype(float), s=50, cmap="RdBu", alpha=0.5 )
 
 plt.show()
-plt.show()
+"""
 
+
+df_final.to_csv("./ListaStudentCluster.csv", sep=',', index=False,)
+ClusterFile = [tuple(row) for row in csv.reader(open("./ListaStudentCluster.csv", 'r'))]
+
+
+
+for i in range(1, len(ClusterFile)):
+
+    if int(ClusterFile[i][4]) == 0:
+        count0 = count0 +1
+        if int(ClusterFile[i][3]) > 0:
+            countFc0 = countFc0 + 1
+    if int(ClusterFile[i][4]) == 1:
+        count1 = count1 +1
+        if int(ClusterFile[i][3]) > 0:
+            countFc1 = countFc1 + 1
+    if int(ClusterFile[i][4]) == 2:
+        count2 = count2 +1
+        if int(ClusterFile[i][3]) > 0:
+            countFc2 = countFc2 +1
+    if int(ClusterFile[i][4]) == 3:
+        count3 = count3 +1
+        if int(ClusterFile[i][3]) > 0:
+         countFc3 = countFc3 + 1
+
+
+media0=countFc0/count0
+media1=countFc1/count1
+media2=countFc2/count2
+media3=countFc3/count3
+
+print("Cluster media fc 0 1 2 3 :")
+print(media0,media1,media2,media3)
+
+print("cluster0", count0)
